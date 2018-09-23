bf317b7f6198e28dcc182d42c03244f031634a9c
Robert Smits <robertsmits@hotmail.be>
2018-09-23 16:28:04 +0200

    Switch DI to Microsoft DependencyInjection

---
+++ VrtNuDownloader/DependencyInjectionConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using VrtNuDownloader.Service;
+using VrtNuDownloader.Service.Interface;
+
+namespace VrtNuDownloader
+{
+    public class DependencyInjectionConfig
+    {
+        public static void RegisterTypes(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ILogService, LogService>();
+            serviceCollection.AddSingleton<IFileService, FileService>();
+            serviceCollection.AddSingleton<IVrtNuService, VrtNuService>();
+            serviceCollection.AddSingleton<IFfmpegService, FfmpegService>();
+            serviceCollection.AddSingleton<IConfigService, ConfigService>();
+            serviceCollection.AddSingleton<IHistoryService, HistoryService>();
+            serviceCollection.AddSingleton<IVrtTokenService, VrtTokenService>();
+            serviceCollection.AddSingleton<IDatabaseService, DatabaseService>();
+            serviceCollection.AddTransient<EpisodeDownloader>();
+        }
+
+        private static Lazy<ServiceProvider> container =
+          new Lazy<ServiceProvider>(() =>
+          {
+              var serviceCollection = new ServiceCollection();
+              RegisterTypes(serviceCollection);
+              return serviceCollection.BuildServiceProvider();
+          });
+
+        public static ServiceProvider Container => container.Value;
+    }
+}
